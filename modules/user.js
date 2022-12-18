4f6757e38f050e2edf5cab339acc19e536cf43b2
Ahmed Ali Klay <ahmedaliklay@outlook.com>
2022-12-18 08:50:52 +0200

    hashpassowrd and comparePassword function to login and register

--- modules/user.js
+++ modules/user.js
@@ -20,5 +20,9 @@ userSchema.methods.hashPassword = function(password){
     return bcrypt.hashSync(password, bcrypt.genSaltSync(5),null);
 }
 
+userSchema.methods.comparePassword = function(password){
+    return bcrypt.compareSync(password,this.password);
+}
+
 
 module.exports = mongoose.model('User',userSchema);
